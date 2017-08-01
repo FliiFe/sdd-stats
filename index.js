10f4bffb705e2670c7fcc0770aec4c13645ff49d
FliiFe <theophile.cailliau@gmail.com>
2017-08-01 22:08:43 +0200

    Now saves the dump into a file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,10 +7,27 @@ client.login(process.env.DISCORD_TOKEN);
 let sddMessages = {};
 
 client.on('ready', () => {
+    console.log('Connected');
     /** @type {Discord.Guild} */
     const sdd = client.guilds.filter(e => e.name === 'Salon des développeurs').first();
-    const chans = sdd.channels.filter(e => e instanceof Discord.TextChannel);
-    chans.map(chan => fetchAllMessages(chan, messages => sddMessages[chan.name] = messages));
+    const chans = sdd.channels.filter(e => e instanceof Discord.TextChannel && e.name !== 'bots');
+    chans.map(chan => fetchAllMessages(chan, messages => {
+        sddMessages[chan.name] = messages.map(m => {
+            return {
+                id: m.id,
+                content: m.content,
+                author: m.author.id,
+                date: m.createdTimestamp
+            };
+        });
+        console.log('\r' + chan.name, 'fetched');
+        process.stdout.write(Object.keys(sddMessages).length + '/' + chans.size);
+        if (Object.keys(sddMessages).length === chans.length) {
+            fs.writeFileSync('./sddMessages.json', JSON.stringify(sddMessages));
+            process.stdout.write('\n');
+            process.exit(0);
+        }
+    }));
 });
 
 /**
@@ -30,11 +48,14 @@ const fetchAllMessages = (channel, finalCallback) => {
     let fullList = new Discord.Collection();
     const fetchCallback = messages => {
         fullList = fullList.concat(messages);
-        // console.log(fullList.size);
-        if(messages.size === 100) channel.fetchMessages({
+        if (messages.size === 100) channel.fetchMessages({
             limit: 100,
-            before: messages.last().id}).then(fetchCallback);
+            before: messages.last().id
+        }).then(fetchCallback);
+        // If size is less than 100, we fetched all the messages
         else finalCallback(fullList.array());
     };
-    channel.fetchMessages({limit: 100}).then(fetchCallback);
+    channel.fetchMessages({
+        limit: 100
+    }).then(fetchCallback);
 };
